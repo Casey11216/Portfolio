9119783e033b8fc0290877022b70d2bea8930c63
Casey11216 <good2gojodele@gmail.com>
2025-04-05 07:10:41 -0700

    Initial commit

---
+++ src/utilities/variables/common/skills/skill.variables.ts
@@ -0,0 +1,127 @@
+import { skill } from "@utilities/types/skills/skill.types";
+/**
+ * Front-End skills
+ */
+export const frontEndSkills: skill = [
+  {
+    skill: "React",
+    level: "Advanced",
+  },
+  {
+    skill: "Next.js",
+    level: "Advanced",
+  },
+  {
+    skill: "Angular",
+    level: "Advanced",
+  },
+  {
+    skill: "Vue.js",
+    level: "Advanced",
+  },
+  {
+    skill: "Svelte",
+    level: "Advanced",
+  },
+  {
+    skill: "React native",
+    level: "Advanced",
+  },
+   {
+    skill: "Nuxt.js",
+    level: "Advanced",
+  },
+  {
+    skill: "Tailwind CSS",
+    level: "Advanced",
+  },
+ 
+  
+  
+];
+/**
+ * Back-End skills
+ */
+export const backEndSkills: skill = [
+  {
+    skill: "Node.js",
+    level: "Advanced",
+  },
+   {
+    skill: "Laravel/PHP",
+    level: "Advanced",
+  },
+  {
+    skill: "Python",
+    level: "Advanced",
+  },
+  {
+    skill: "Django",
+    level: "Advanced",
+  },
+  {
+    skill: "Flask",
+    level: "Advanced",
+  },
+  {
+    skill: "FastAPI",
+    level: "Advanced",
+  },
+  {
+    skill: "SQL",
+    level: "Intermediate",
+  },
+  {
+    skill: "MongoDB",
+    level: "Intermediate",
+  },
+  {
+    skill: "PostgreSQL",
+    level: "Intermediate",
+  },
+];
+
+// AI skills
+export const aiSkills: skill = [
+  {
+    skill: "AI",
+    level: "Advanced",
+  },
+  {
+    skill: "Machine learing",
+    level: "Advanced",
+  },
+  {
+    skill: "Deep learning",
+    level: "Advanced",
+  },
+  {
+    skill: "NLP",
+    level: "Advanced",
+  },
+  {
+    skill: "TensorFlow",
+    level: "Advanced",
+  },
+];
+// CMS skills
+export const cmsSkills: skill = [
+  {
+    skill: "WordPress",
+    level: "Advanced",
+  },
+  {
+    skill: "Elementor",
+    level: "Advanced",
+  },
+  {
+    skill: "Divi",
+    level: "Advanced",
+  },
+  {
+    skill: "Woocommerce",
+    level: "Advanced",
+  },
+
+];
+
